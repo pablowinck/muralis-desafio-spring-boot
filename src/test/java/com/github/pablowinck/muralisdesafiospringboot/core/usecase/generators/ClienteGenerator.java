5ed01a4f854783dc416ebe9008b3ce6d397609aa
pablowinck <pablowinck123@gmail.com>
2023-01-04 07:28:25 -0300

    adiciona numero no factory de clientes

--- src/test/java/com/github/pablowinck/muralisdesafiospringboot/core/usecase/generators/ClienteGenerator.java
+++ src/test/java/com/github/pablowinck/muralisdesafiospringboot/core/usecase/generators/ClienteGenerator.java
@@ -9,18 +9,19 @@ import java.util.List;
 public class ClienteGenerator {
     private static final List<String> names = List.of("John Doe", "Jane Doe", "John Smith", "Jane Smith");
     private static final List<String> ceps = List.of("12345678", "98765432", "21436578", "89674532");
+    private static final List<String> numeros = List.of("123", "456", "789", "012");
 
     public static Cliente generate() {
         return Cliente.builder()
                 .nome(names.get(randomIndex(names.size())))
-                .endereco(Endereco.builder().cep(ceps.get(randomIndex(ceps.size()))).build())
+                .endereco(Endereco.builder().cep(ceps.get(randomIndex(ceps.size()))).numero(numeros.get(randomIndex(numeros.size()))).build())
                 .build();
     }
 
     public static Cliente generate(String nome) {
         return Cliente.builder()
                 .nome(nome)
-                .endereco(Endereco.builder().cep(ceps.get(randomIndex(ceps.size()))).build())
+                .endereco(Endereco.builder().cep(ceps.get(randomIndex(ceps.size()))).numero(numeros.get(randomIndex(numeros.size()))).build())
                 .build();
     }
 
@@ -28,6 +29,7 @@ public class ClienteGenerator {
         return CadastraClienteDto.builder()
                 .nome(names.get(randomIndex(names.size())))
                 .cep(ceps.get(randomIndex(ceps.size())))
+                .numero(numeros.get(randomIndex(numeros.size())))
                 .build();
     }
 
