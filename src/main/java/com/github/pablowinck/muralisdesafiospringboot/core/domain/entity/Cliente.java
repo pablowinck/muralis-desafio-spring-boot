32ac6198597798e4809973211f10d2e109a24018
pablowinck <pablowinck123@gmail.com>
2023-01-04 07:28:01 -0300

    corrige importacao lombok

--- src/main/java/com/github/pablowinck/muralisdesafiospringboot/core/domain/entity/Cliente.java
+++ src/main/java/com/github/pablowinck/muralisdesafiospringboot/core/domain/entity/Cliente.java
@@ -1,10 +1,7 @@
 package com.github.pablowinck.muralisdesafiospringboot.core.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.util.ArrayList;
@@ -13,6 +10,7 @@ import java.util.Objects;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
