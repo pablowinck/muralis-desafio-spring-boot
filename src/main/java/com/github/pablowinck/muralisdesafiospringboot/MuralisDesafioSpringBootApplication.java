472fdf9be0f585992d122cbc3f9f1e873acc76f5
pablowinck <pablowinck123@gmail.com>
2023-01-04 04:19:50 -0300

    Initial commit

---
+++ src/main/java/com/github/pablowinck/muralisdesafiospringboot/MuralisDesafioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.pablowinck.muralisdesafiospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuralisDesafioSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuralisDesafioSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/pablowinck/muralisdesafiospringboot/MuralisDesafioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pablowinck.muralisdesafiospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuralisDesafioSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
